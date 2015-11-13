f0263284f5bf5b517745a893b3a34faff8862c54
linsolas <linsolas@gmail.com>
2015-11-13 23:27:19 +0100

    feat: add timestramp in message

--- index.js
+++ index.js
@@ -12,33 +12,34 @@ module.exports = zeJarvisChatServer
 //
 
 function zeJarvisChatServer (initialDB, options) {
-  options = options || {}
+  options = options || {};
 
-  var app = express()
-  var db = initialDB || []
+  var app = express();
+  var db = initialDB || [];
 
   if (options.verbose) {
-    app.use(morgan('dev'))
+    app.use(morgan('dev'));
   }
 
-  app.use(cors())
-  app.use(bodyParser.json())
+  app.use(cors());
+  app.use(bodyParser.json());
 
   app.get('/messages', function (req, res) {
     res.json(db.map(function (value, id) {
       return Object.assign({}, value, {
         _id: String(id)
-      })
-    }))
-  })
+      });
+    }));
+  });
 
   app.post('/messages', function (req, res) {
-    var newEntry = req.body
-    db.unshift(newEntry)
+    var newEntry = req.body;
+    newEntry.timestamp = Date.now();
+    db.unshift(newEntry);
     res
       .status(201)
-      .json(Object.assign({ _id: db.length }, newEntry))
-  })
+      .json(Object.assign({ _id: db.length }, newEntry));
+  });
 
   return app
 }
